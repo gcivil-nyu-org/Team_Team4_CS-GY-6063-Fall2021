3dd11a165378f6a18594a92e16fd24e7b3e6cbd1
leowang <leowang9863@gmail.com>
2021-10-08 18:56:21 -0400

    added accounts app

---
+++ accounts/views.py
@@ -0,0 +1,50 @@
+# views.py
+from django.shortcuts import render, redirect
+from .forms import RegisterForm
+
+from django.contrib.auth import authenticate, login, logout
+
+from django.contrib import messages
+# Create your views here.
+from django.contrib.auth.decorators import login_required
+
+
+def registerPage(request):
+    form = RegisterForm()
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get("username")
+            messages.success(request, "Account was created for"+user)
+            return redirect("login")
+
+    return render(request, "accounts/register.html", {"form": form})
+
+
+def loginPage(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('user')
+        else:
+            messages.info(request, 'Username OR password is incorrect')
+
+    context = {}
+    return render(request, 'accounts/login.html', context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
+
+
+@login_required(login_url='login')
+def user(request):
+    return render(request, 'accounts/user.html')
