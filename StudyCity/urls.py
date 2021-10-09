42531f768d47b8080d9eba7c4c94db1a891660e5
leowang <leowang9863@gmail.com>
2021-10-08 20:37:03 -0400

    added index page

--- StudyCity/urls.py
+++ StudyCity/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-
+from . import views
 
 urlpatterns = [
+
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
 ]
