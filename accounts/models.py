56143cc345b766b191a94630edaab6c3f783805e
Scott Canning <sc7979@nyu.edu>
2021-10-11 16:34:52 -0400

    registration

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    username = models.CharField(max_length=256)
+    email = models.CharField(max_length=256)
+    business_account = models.BooleanField(default=False)
+    def __str__(self):
+        return self
+    def set(self, username, email, business_account):
+        self.username = username
+        self.email = email
+        self.business_account = business_account
