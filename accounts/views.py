e240b0f03c190dd6ac1e4e957fe450e22a90c984
zhanghang <hz2447@nyu.edu>
2021-10-11 21:09:08 -0400

    add updating profile functionality

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,7 @@
 from django.shortcuts import render, redirect
 
 from accounts.models import Profile
-from .forms import RegisterForm
+from .forms import RegisterForm, UserUpdateForm, ProfileUpdateForm
 
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
@@ -66,4 +66,24 @@ def user(request):
 
 @login_required
 def profile(request):
-    return render(request, 'accounts/profile.html')
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST,
+                                   request.FILES,
+                                   instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your account has been updated!')
+            return redirect('profile')
+
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+
+    return render(request, 'accounts/profile.html',context)
