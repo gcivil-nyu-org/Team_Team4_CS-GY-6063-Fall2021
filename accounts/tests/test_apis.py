f8b23b5f1ca11d9245e6c73365d8a19f69a6b813
Scott-Canning <34758484+Scott-Canning@users.noreply.github.com>
2021-10-26 21:07:51 -0400

    yelp tests

--- accounts/tests/test_apis.py
+++ accounts/tests/test_apis.py
@@ -1,14 +1,11 @@
-from unittest.main import TestProgram
 from django.test import SimpleTestCase
-from django.urls import reverse, resolve
-from accounts.yelp_api import open_data_query
-from accounts.open_data_api import yelp_search
+from accounts.yelp_api import yelp_search
+# from accounts.open_data_api import  open_data_query
 
 class StudyCityAPITests(SimpleTestCase):
 
     def test_yelp_api(self):
         test_yelp_query = yelp_search()
-
 
         term = 'tandon'
         location = 'new york'
@@ -17,16 +14,16 @@ class StudyCityAPITests(SimpleTestCase):
         url = 'https://api.yelp.com/v3/businesses/E8RJkjfdcwgtyoPMjQ_Olg'
 
         
-        response = self.test_yelp_query.search_location(term, location)
+        response = test_yelp_query.search_location(term, location)
         self.assertEqual(response.status_code, 200)
         
-        response = self.test_yelp_query.filter_location(params)
+        response = test_yelp_query.filter_location(params)
         self.assertEqual(response.status_code, 200)
 
-        response = self.test_yelp_query.search_business_id(business_id)
+        response = test_yelp_query.search_business_id(business_id)
         self.assertEqual(response.status_code, 200)
 
-        response = self.test_yelp_query.request(url)
+        response = test_yelp_query.request(url)
         self.assertEqual(response.status_code, 200)
      
     def test_open_data_api(self):
