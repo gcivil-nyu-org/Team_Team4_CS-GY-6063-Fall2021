841b9d0cd58ab5db0762df382fd57ed1c6b72d51
zhanghang <hz2447@nyu.edu>
2021-10-27 02:15:47 -0400

    add unittest for favorite form

--- accounts/tests/test_forms.py
+++ accounts/tests/test_forms.py
@@ -1,20 +1,20 @@
 from django.test import SimpleTestCase
-from accounts.forms import ReviewCreateForm
+from accounts.forms import ReviewCreateForm, FavoriteCreateForm
 from django.contrib.auth.models import User
 
 
 class TestForms(SimpleTestCase):
     databases = '__all__'
 
-    def setUp(self):
-        self.test_user = User.objects.create(
+    # def setUp(self):
+
+    def test_review_form(self):
+        test_user = User.objects.create(
             username='test_user',
             email='abc@gmail.com',
             password='zh000000')
-
-    def test_review_form(self):
         form = ReviewCreateForm(data={
-            'user': self.test_user,
+            'user': test_user,
             'yelp_id': 'abcdefg',
             'business_name': 'cafe star',
             'review_text': 'Good place to go!',
@@ -23,5 +23,17 @@ class TestForms(SimpleTestCase):
             'comfort_rating': 3,
             'food_rating': 4,
             'charging_rating': 5
+        })
+        self.assertTrue(form.is_valid())
+
+    def test_favorite_form(self):
+        test_user = User.objects.create(
+            username='test_user1',
+            email='abc1@gmail.com',
+            password='zh000000')
+        form = FavoriteCreateForm(data={
+            'user': test_user,
+            'yelp_id': 'abcdefg',
+            'business_name': 'cafe star'
         })
         self.assertTrue(form.is_valid())
