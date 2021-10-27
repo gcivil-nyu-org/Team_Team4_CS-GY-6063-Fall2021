e9f9ada8a1506023543d480f2be9d11bdd94c282
Scott-Canning <34758484+Scott-Canning@users.noreply.github.com>
2021-10-27 14:13:31 -0400

    all url tests

--- accounts/tests/test_urls.py
+++ accounts/tests/test_urls.py
@@ -1,5 +1,6 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
+from django.template.loader import render_to_string
 from accounts.views import index, loginPage, registerPage, \
                            logoutUser, user, profile, locationDetail
                         
@@ -39,3 +40,23 @@ class StudyCityURLTests(SimpleTestCase):
     def test_url_location_detail(self):
         url = reverse('locationDetail')
         self.assertEquals(resolve(url).func, locationDetail)
+
+
+    def test_url_password_reset(self):
+        with self.assertTemplateUsed(template_name='accounts/password_reset.html'):
+            render_to_string('accounts/password_reset.html')
+
+
+    def test_url_password_reset_done(self):
+        with self.assertTemplateUsed(template_name='accounts/password_reset_done.html'):
+            render_to_string('accounts/password_reset_done.html')
+
+
+    def test_url_password_reset_confirm(self):
+        with self.assertTemplateUsed(template_name='accounts/password_reset_confirm.html'):
+            render_to_string('accounts/password_reset_confirm.html')
+
+
+    def test_url_password_reset_complete(self):
+        with self.assertTemplateUsed(template_name='accounts/password_reset_complete.html'):
+            render_to_string('accounts/password_reset_complete.html')
