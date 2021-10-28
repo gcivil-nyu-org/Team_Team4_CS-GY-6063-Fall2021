611aa8003241bcb7c196afc6496f1a8a5cd59a89
leowang <leowang9863@gmail.com>
2021-10-27 23:36:24 -0400

    updated staticfiles dir

--- StudyCity/settings.py
+++ StudyCity/settings.py
@@ -127,7 +127,8 @@ USE_TZ = True
 STATIC_URL = "/static/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
