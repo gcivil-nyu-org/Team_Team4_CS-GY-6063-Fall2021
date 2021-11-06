f704f17544a514892aa134c7b7656b13d4b3ee7a
zhanghang <hz2447@nyu.edu>
2021-11-06 16:06:39 -0400

    test coveralls

--- accounts/urls.py
+++ accounts/urls.py
@@ -13,6 +13,26 @@ urlpatterns = [
     path('user/', views.user, name="user"),
     path('profile/', views.profile, name="profile"),
     path('location', views.locationDetail, name="locationDetail"),
+    # path('password-reset/',
+    #      auth_views.PasswordResetView.as_view(
+    #          template_name='accounts/password_reset.html'
+    #      ),
+    #      name='password_reset'),
+    # path('password-reset/done/',
+    #      auth_views.PasswordResetDoneView.as_view(
+    #          template_name='accounts/password_reset_done.html'
+    #      ),
+    #      name='password_reset_done'),
+    # path('password-reset-confirm/<uidb64>/<token>/',
+    #      auth_views.PasswordResetConfirmView.as_view(
+    #          template_name='accounts/password_reset_confirm.html'
+    #      ),
+    #      name='password_reset_confirm'),
+    # path('password-reset-complete/',
+    #      auth_views.PasswordResetCompleteView.as_view(
+    #          template_name='accounts/password_reset_complete.html'
+    #      ),
+    #      name='password_reset_complete'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
