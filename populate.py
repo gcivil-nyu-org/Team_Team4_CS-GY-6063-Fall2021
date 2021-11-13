8f42b3911018f61c911a98cea69cb7d08c8e650d
zhanghang <hz2447@nyu.edu>
2021-11-12 23:53:14 -0500

    Automatically create 7 test accounts

---
+++ populate.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import sys
+import os
+import django
+
+cur_path = os.getcwd()
+sys.path.append(cur_path)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'StudyCity.settings'
+django.setup()
+
+from accounts.models import Profile
+from django.contrib.auth.models import User
+
+usernames = ["prof_test","ta_test","test1","test2","test3","test4","test_5"]
+business = [True, False, True, True, False, False, False]
+password = "studycity"
+for idu, user_name in enumerate(usernames):
+    user = User.objects.filter(username = user_name)
+    if not user:
+        user = User.objects.create(username = user_name, password = password, email = user_name+"@gmail.com")
+        print("Create "+ user_name+"!")
+        user.save()
+    else:
+        user = user[0]
+    Profile.objects.filter(user=user).update(business_account=business[idu]) 
+
+    
