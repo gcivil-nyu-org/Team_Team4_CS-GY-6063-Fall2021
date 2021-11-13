61a998e9f3172db2e00d3fd446e5b45e15d77508
zhanghang <hz2447@nyu.edu>
2021-11-13 00:13:31 -0500

    config flake8

--- populate.py
+++ populate.py
@@ -9,7 +9,6 @@ cur_path = os.getcwd()
 sys.path.append(cur_path)
 os.environ['DJANGO_SETTINGS_MODULE'] = 'StudyCity.settings'
 django.setup()
-
 from accounts.models import Profile
 from django.contrib.auth.models import User
 
@@ -19,10 +18,9 @@ password = "studycity"
 for idu, user_name in enumerate(usernames):
     user = User.objects.filter(username = user_name)
     if not user:
-        user = User.objects.create(username = user_name, password = password, email = user_name+"@gmail.com")
+        user = User.objects.create(username = user_name,
+            password = password, email = user_name+"@gmail.com")
         user.save()
     else:
         user = user[0]
     Profile.objects.filter(user=user).update(business_account=business[idu]) 
-
-    
