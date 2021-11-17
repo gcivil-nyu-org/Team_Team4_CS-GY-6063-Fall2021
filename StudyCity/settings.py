d56d7d5b0fa196a1d698c668b85193c233da9d53
zhanghang <hz2447@nyu.edu>
2021-11-16 20:32:58 -0500

    aws S3 for file upload

--- StudyCity/settings.py
+++ StudyCity/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "accounts.apps.AccountsConfig",
     "crispy_forms",
+    "storages",
 ]
 
 MIDDLEWARE = [
@@ -154,3 +155,12 @@ EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASS")
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 if '/app' in os.environ['HOME']:
     django_heroku.settings(locals())
+
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_S3_SIGNATURE_VERSION = "s3v4"
+AWS_S3_REGION_NAME = "us-east-2"
