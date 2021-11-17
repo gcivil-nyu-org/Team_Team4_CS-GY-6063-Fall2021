621757a2be03c3fbba248ee65c4a51a79f9e7add
Kathy Ching <kc3191@nyu.edu>
2021-11-17 09:18:29 -0500

    fixed a few bugs. added env.

---
+++ staticfiles/js/index.b58ff34277d2.js
@@ -0,0 +1,28 @@
+function showPosition() {
+  if (navigator.geolocation) {
+    document.getElementById(
+      "locationLoading"
+    ).innerHTML = `<i class="fas fa-spinner"></i>Getting your location...`;
+    document.getElementById("searchLocation").disabled = true;
+    navigator.geolocation.getCurrentPosition(function (position) {
+      document.getElementById("longitude").value = position.coords.longitude;
+      document.getElementById("latitude").value = position.coords.latitude;
+      document.getElementById("locationLoading").innerHTML = "";
+      document.getElementById("searchLocation").disabled = false;
+    });
+  } else {
+    alert("Sorry, your browser does not support HTML5 geolocation.");
+  }
+}
+
+function toggleCurrentLocation() {
+  var currentLocation = document.getElementById("useCurrentLocation");
+  if (currentLocation.checked) {
+    showPosition();
+    currentLocation.value = "true";
+  } else {
+    currentLocation.value = "false";
+    document.getElementById("locationLoading").innerHTML = "";
+    document.getElementById("searchLocation").disabled = false;
+  }
+}
