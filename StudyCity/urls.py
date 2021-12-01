77909a44463d603e2ac597f75ef9b0e9b4bc25c1
zhanghang <hz2447@nyu.edu>
2021-11-30 21:37:15 -0500

    removed useless import

--- StudyCity/urls.py
+++ StudyCity/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-from django.conf import settings
-from django.conf.urls.static import static
+# from django.conf import settings
+# from django.conf.urls.static import static
 
 urlpatterns = [
 
